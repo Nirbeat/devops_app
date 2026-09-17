d3bc07de60033f4fa8b7fae12e40f8ed74ee1ce5
Noxie-PC <maximilianomartindev@google.com>
2026-09-16 22:48:17 -0300

    prueba 3 de ci-cd

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use('/api/auth', authRoutes);
 
 // Este endpoint está comentado para ser re-integrado en un commit
 app.get('/health', (req, res) => {
-  res.json({ status: 'ok', uptime: process.uptime(), update: "intento 2 ci-cd" });
+  res.json({ status: 'ok', uptime: process.uptime(), update: "intento 3 ci-cd" });
 });
 
 app.use((err, req, res, next) => {
