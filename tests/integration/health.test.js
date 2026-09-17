7a9187b359041284229e467e3a2de148d63908ac
Noxie-PC <maximilianomartindev@google.com>
2026-09-17 20:51:12 -0300

    health agregado

--- tests/integration/health.test.js
+++ tests/integration/health.test.js
@@ -3,17 +3,17 @@ import { expect } from 'chai';
 import request from 'supertest';
 import app from '../../src/app.js';
 
-// describe('Endpoint de salud GET /health', () => {
-//   it('debe devolver 200 y un status "ok"', async () => {
-//     const res = await request(app).get('/health');
-//     expect(res.status).to.equal(200);
-//     expect(res.body.status).to.equal('ok');
-//   });
+describe('Endpoint de salud GET /health', () => {
+    it('debe devolver 200 y un status "ok"', async () => {
+        const res = await request(app).get('/health');
+        expect(res.status).to.equal(200);
+        expect(res.body.status).to.equal('ok');
+    });
 
-//   it('debe incluir un campo uptime numérico', async () => {
-//     const res = await request(app).get('/health');
-//     expect(res.body).to.have.property('uptime');
-//     expect(res.body.uptime).to.be.a('number');
-//     expect(res.body.uptime).to.be.greaterThan(0);
-//   });
-// });
+    it('debe incluir un campo uptime numérico', async () => {
+        const res = await request(app).get('/health');
+        expect(res.body).to.have.property('uptime');
+        expect(res.body.uptime).to.be.a('number');
+        expect(res.body.uptime).to.be.greaterThan(0);
+    });
+});
